dc3227dd1363c6d5cabd8691a962b1ad887eaf9f
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-10 14:19:15 -0300

    24° commit - logica read implementada

--- src/pages/Guis/index.js
+++ src/pages/Guis/index.js
@@ -1,8 +1,17 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import React, {useState, useEffect} from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function Guis(){
+
+  const[my_guis, setGuis] = useState([]);
+  const navigate = useNavigate();
+
+  // Read, lista todos os registros da api
+  useEffect(() => {
+    api.get('api/v1/guis',{})
+    .then(response => {setGuis(response.data)})
+  }, []);
 
   return(
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -17,27 +26,30 @@ export default function Guis(){
         <table data-testid="mytable" className="table table-hover">
           <thead>
             <tr>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
+              <th scope="col">Id</th>
+              <th scope="col">Projeto</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row"></th>
-                <td></td>
-                <td></td>
-                <td>
+            {my_guis.map(gui => (
+              <tr key={gui.id}>
+                <th scope="row">{gui.id}</th>
+                  <td>{gui.project}</td>
+                  <td>{gui.description}</td>
+                  <td>
 
-                  <button data-testid="mybtn1" type="button"
-                  className="btn btn-outline-info">Editar</button>
+                    <button data-testid="mybtn1" type="button"
+                    className="btn btn-outline-info">Editar</button>
 
-                  <button data-testid="mybtn2" type="button"
-                  className="btn btn-outline-danger">Excluir</button>
+                    <button data-testid="mybtn2" type="button"
+                    className="btn btn-outline-danger">Excluir</button>
 
-                </td>
-            </tr>
+                  </td>
+              </tr>
+            ))}
+            
           </tbody>
         </table>
 
